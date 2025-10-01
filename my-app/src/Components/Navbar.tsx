fbff853f7da65be2161e6a639e002357588e1a6a
Adarsh Sudheer <117463998+adarsh2023calc@users.noreply.github.com>
2025-10-01 10:39:09 +0000

    Some random changes

---
+++ my-app/src/Components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./Navbar.css"; // import CSS file
+
+import LoginModal from "./Login";
+
+export default function Navbar() {
+   const [isLoginOpen, setIsLoginOpen] = useState(false);
+  return (
+    <nav className="navbar">
+      {/* Logo */}
+      <div className="logo">JeePrep</div>
+
+      {/* Nav Links */}
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ul>
+
+      {/* Auth Links */}
+      <ul className="auth-links">
+        <li className="signup">Sign Up</li>
+        <li className="login" onClick={() => setIsLoginOpen(true)}>Log In</li>
+      </ul>
+
+      {/* Popup login modal */}
+      <LoginModal isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
+       </nav>
+  );
+}
