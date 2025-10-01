a3192b5e644c3834a54639a0d2b4f7f0013c042d
Adarsh Sudheer <117463998+adarsh2023calc@users.noreply.github.com>
2025-10-01 14:12:32 +0000

    Main Pages

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
-
+import Hero from './Components/HeroPage'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
