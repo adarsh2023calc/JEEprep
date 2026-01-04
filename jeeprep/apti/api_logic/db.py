696295ea34fda56ed3406df32c46524cbcba440e
Adarsh Sudheer <117463998+adarsh2023calc@users.noreply.github.com>
2026-01-04 17:33:35 +0000

    modified:   jeeprep/apti/api_logic/db.py 	modified:   jeeprep/apti/templates/aptitude_quiz.html 	modified:   jeeprep/apti/templates/coding_questions.html 	new file:   jeeprep/apti/templates/error_logs 	modified:   jeeprep/apti/templates/software_quiz.html 	modified:   jeeprep/apti/templates/verbals_quiz.html

--- jeeprep/apti/api_logic/db.py
+++ jeeprep/apti/api_logic/db.py
@@ -20,10 +20,10 @@ def save_to_bigquery(user_id, assessment_id, data, purpose):
 
     
     data = json.loads(data)
-    
-    if purpose == "Coding":
+    rows=[]
+    if purpose == "coding":
         data = data.get("output")
-        rows=[]
+        
         table_id = "locenergy.jeeprep.coding_problems"
         for key in data:
             row = {
@@ -35,28 +35,34 @@ def save_to_bigquery(user_id, assessment_id, data, purpose):
                 "topics": key.get("topics", []),
                 "testcases": key.get("testcases", []),
                 "boiler_plate_code": key.get("boiler_plate_code"),
+
             }
             rows.append(row)
-        errors = client.insert_rows_json(
-            table_id,
-            rows  # must always be a list
-        )
+        
 
     else:
         
-        table_id = "locenergy.jeeprep.user_assessment"
+        table_id = "locenergy.jeeprep.assessment_questions"
+        rows=[]
+        Questions=data.get("questions")
+        for key in Questions:
+            row = {
+                "user_id": user_id,
+                "assessment_id": assessment_id,
+                "question": key.get("question"),
+                "options": key.get('options'),
+                "difficulty": key.get("difficulty"),     
+                "topic": key.get('topic')[0],
+                "created_at": datetime.utcnow().isoformat(),
+                "answer": key.get("answer"),
+                "purpose":purpose
+            }
+            rows.append(row)
 
-        row = {
-            "user_id": user_id,
-            "assessment_id": assessment_id,
-            "questions": data.get("questions"),        
-            "created_at": datetime.utcnow(),
-        }
-
-        errors = client.insert_rows_json(
+    errors = client.insert_rows_json(
             table_id,
-            [row]   # must always be a list
-        )
+            rows   # must always be a list
+    )
 
     if errors:
         print("Insert errors:", errors)
