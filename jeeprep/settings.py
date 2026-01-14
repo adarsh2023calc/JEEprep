03cf16743281144a6779ae30c578ac70765d887b
Adarsh Sudheer <117463998+adarsh2023calc@users.noreply.github.com>
2026-01-14 18:02:30 +0000

    modified:   jeeprep/settings.py 	modified:   requirements.txt

--- jeeprep/settings.py
+++ jeeprep/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = "jeeprep.urls"
@@ -135,6 +136,7 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
